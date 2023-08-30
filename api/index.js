24decbf8a36da29d1d6e857ccfbf14f2615eb9d0
taahirahismail <taahirah.i@gmail.com>
2023-08-30 12:02:36 +0200

    getting started

---
+++ api/index.js
@@ -0,0 +1,42 @@
+const {express, routes} = require('./controllers');
+const errorHandling = require('./middleware/errorHandling');
+
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// cors access
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next();
+});
+
+app.use(
+    express.static('./static'),
+    express.urlencoded({
+        extended: false
+    }),
+    cookieParser(),
+    cors(),
+    routes,
+    errorHandling
+);
+
+// direct to home page with routes descriptions
+routes.get('/',
+(req, res) => {
+    res.sendFile(path.resolve(__dirname, './static/html/index.html'));
+})
+
+// set server
+app.listen(port, () => {
+    console.log(`Moonstruck Vinyl's server running on port http://localhost:${port}`)
+});
