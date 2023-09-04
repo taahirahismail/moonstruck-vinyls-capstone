1585699b1b4cc484598e1ec16d29ecfca2e7461a
taahirahismail <taahirah.i@gmail.com>
2023-09-04 16:13:19 +0200

    done for the day

---
+++ frontend/src/components/AddProductComp.vue
@@ -0,0 +1,213 @@
+<template>
+  <div>
+    <div class="button">
+      <a
+        data-bs-toggle="modal"
+        data-bs-target="#add-prod-modal"
+        href="#add-prod-modal"
+        class="m-2 text-center btn add-prod-btn"
+      >
+        Add Product
+      </a>
+    </div>
+
+    <div class="modal" id="add-prod-modal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="text-center heading-text">New Product:</h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+            ></button>
+          </div>
+
+          <div>
+            <form class="p-3 purple-bg">
+              <div class="mb-3">
+                <label for="prodID" class="form-label"> Product ID: * </label>
+                <input
+                  v-model="model.product.prodID"
+                  type="text"
+                  id="prodID"
+                  class="form-control input-bg"
+                  placeholder="eg: MUS001"
+                  required
+                  oninvalid="this.setCustomValidity('Please enter a product ID')"
+                  oninput="this.setCustomValidity('')"
+                />
+              </div>
+
+              <div class="mb-3">
+                <label for="albumName" class="form-label">
+                  Album Name: *
+                </label>
+                <input
+                  v-model="model.product.albumName"
+                  type="text"
+                  id="albumName"
+                  class="form-control input-bg"
+                  placeholder="eg: This Album"
+                  required
+                  oninvalid="this.setCustomValidity('Please enter an album name')"
+                  oninput="this.setCustomValidity('')"
+                />
+              </div>
+
+              <div class="mb-3">
+                <label for="albumArtist" class="form-label">
+                  Album Artist: *
+                </label>
+                <input
+                  v-model="model.product.albumArtist"
+                  type="text"
+                  id="albumArtist"
+                  class="form-control input-bg"
+                  placeholder="eg: Musicians"
+                  required
+                  oninvalid="this.setCustomValidity('Please enter an album artist.')"
+                  oninput="this.setCustomValidity('')"
+                />
+              </div>
+
+              <div class="mb-3">
+                <label for="genre" class="form-label"> Genre: * </label>
+                <input
+                  v-model="model.product.prodID"
+                  type="text"
+                  id="genre"
+                  class="form-control input-bg"
+                  placeholder="eg: pop"
+                  required
+                  oninvalid="this.setCustomValidity('Please enter genre.')"
+                  oninput="this.setCustomValidity('')"
+                />
+              </div>
+
+              <div class="mb-3">
+                <label for="price" class="form-label"> Price: * </label>
+                <input
+                  v-model="model.product.price"
+                  type="number"
+                  id="price"
+                  class="form-control input-bg"
+                  placeholder="eg: 300.00"
+                  required
+                  oninvalid="this.setCustomValidity('Please enter a price')"
+                  oninput="this.setCustomValidity('')"
+                />
+              </div>
+
+              <div class="mb-3">
+                <label for="quantity" class="form-label"> Quantity: * </label>
+                <input
+                  v-model="model.product.quantity"
+                  type="number"
+                  id="quantity"
+                  class="form-control input-bg"
+                  placeholder="eg: 100"
+                  required
+                  oninvalid="this.setCustomValidity('Please enter quantity')"
+                  oninput="this.setCustomValidity('')"
+                />
+              </div>
+
+              <div class="mb-3">
+                <label for="prodImg" class="form-label"> Product Image: </label>
+                <input
+                  v-model="model.product.prodImg"
+                  type="text"
+                  id="prodImg"
+                  class="form-control input-bg"
+                  placeholder="Insert image link."
+                />
+              </div>
+
+              <div class="mb-3">
+                <label for="albumDesc" class="form-label">
+                  Album Description:
+                </label>
+                <input
+                  v-model="model.product.albumDesc"
+                  type="text"
+                  id="albumDesc"
+                  class="form-control input-bg"
+                  placeholder="Insert album description."
+                />
+              </div>
+
+              <p>* indicates a required field</p>
+            </form>
+          </div>
+
+          <div class="modal-footer">
+            <button
+              type="submit"
+              class="m-2 btn add-prod-btn"
+              data-bs-dismiss="modal"
+              @click="addProduct()"
+            >
+              Add Product
+            </button>
+            <button type="reset" class="m-2 btn clr-btn">Clear Fields</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            model: {
+                product: {
+                    prodID: "",
+                    albumName: "",
+                    albumArtist: "",
+                    genre: "",
+                    price: "",
+                    quantity: "",
+                    prodImg: "",
+                    albumDesc: ""
+                }
+            }
+        }
+    },
+
+    methods: {
+        addProduct() {
+            this.$store.dispatch("addProduct", this.model.product);
+            this.$router.push("/admin");
+            setTimeout(() => {
+                console.log("Reload");
+                location.reload()
+            }, 5000);
+        }
+    }
+};
+</script>
+
+<style>
+.button>a {
+  background: #0A192F;
+  color: #FFD700;
+  border: 3px solid #FFD700;
+  border-radius: 10px;
+  box-shadow: 0 0 0 0 transparent;
+  -webkit-transition: all 0.2s ease-in;
+  -moz-transition: all 0.2s ease-in;
+  transition: all 0.2s ease-in;
+}
+
+.button>a:hover {
+    background: #010111;
+  color: #FFD700;
+  box-shadow: 0 0 30px 5px #FFD700;
+  -webkit-transition: all 0.2s ease-out;
+  -moz-transition: all 0.2s ease-out;
+  transition: all 0.2s ease-out;
+}
+</style>
--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,15 +1,108 @@
 <template>
     <div>
-        
+        <h1 class="text-center p-3 heading-text">Admin !</h1>
+
+        <div>
+            <div class="d-flex justify-content-between">
+                <h3 class="text-center p-3">Products</h3>
+                <add-product-comp/>
+            </div>
+            <div class="d-flex justify-content-center">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Product ID</th>
+                            <th>Album Name</th>
+                            <th>Album Artist</th>
+                            <th>Genre</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Product Image</th>
+                            <th>Album Description</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody v-for="product in products" :key="product.prodID">
+                        <tr v-if="products">
+                            <td>{{ product.prodID }}</td>
+                            <td>{{ product.albumName }}</td>
+                            <td>{{ product.albumArtist }}</td>
+                            <td>{{ product.genre }}</td>
+                            <td>R{{ product.price }}</td>
+                            <td>{{ product.quantity }}</td>
+                            <td class="prod-img">
+                                <img :src="product.prodImg" :alt="product.albumName"
+                                loading="lazy"
+                                class="img-fluid prod-img">
+                            </td>
+                            <td>{{ product.albumDesc }}</td>
+                            <td>
+                                <!-- insert update button here -->
+                                <button class="btn del-btn" @click="deleteProduct(product.prodID)">Delete</button>
+                            </td>
+                        </tr>
+
+                        <tr v-else>
+                            <!-- insert spinner here -->
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import SpinnerComp from "../components/SpinnerComp.vue";
+import AddProductComp from "@/components/AddProductComp.vue";
+
 export default {
-    
+    components: {
+        SpinnerComp,
+        AddProductComp
+    },
+
+    computed: {
+        products() {
+            return this.$store.state.products;
+        },
+        product() {
+            return this.$store.state.product;
+        }
+    },
+
+    mounted() {
+        this.$store.dispatch("fetchProducts");
+    },
+
+    methods: {
+        deleteProduct(prodID) {
+            if (confirm("Are you sure you want to delete this product?")) {
+                this.$store.dispatch("deleteProduct", prodID);
+                setTimeout(() => {
+                    location.reload();
+                }, 500);
+            }
+        }
+    }
 }
 </script>
 
 <style scoped>
-    
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.heading-text {
+  font-family: "Archivo Black", sans-serif;
+}
+
+tr, th, td {
+    border: 3px solid #f4f4f4;
+    color: #f4f4f4;
+    padding: 5px;
+}
 </style>
